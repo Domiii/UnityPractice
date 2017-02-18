3f4b6a96b266dd5edde84c0285ee66549e3db629
Domi <Domiii@users.noreply.github.com>
2017-02-18 15:00:55 +0800

    added MoveToClickPoint

--- Snippets.js
+++ Snippets.js
@@ -421,9 +421,27 @@ void DropOne() {
 }`
   },
   {
-    title_en: '',
+    title_en: 'MoveToClickPoint',
+    tags: ['navmesh'],
     code:
-``
+`using UnityEngine;
+ 
+public class MoveToClickPoint : MonoBehaviour {
+    NavMeshAgent agent;
+
+    void Start() {
+       agent = GetComponent<NavMeshAgent>();
+    }
+ 
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+           RaycastHit hit;
+           if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}`
   },
   {
     title_en: '',
