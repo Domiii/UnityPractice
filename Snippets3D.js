497ee7e5f75e02137effb786e2bde4f1fc225de7
Domi <Domiii@users.noreply.github.com>
2017-02-23 17:31:40 +0800

    added MoveWithTarget

--- Snippets3D.js
+++ Snippets3D.js
@@ -777,10 +777,33 @@ void OnTriggerEnter(Collider collider) {
 }`
   },
   {
-    name: '',
-	title_en: '',
+    name: 'MoveWithTarget',
+	  title_en: 'First add empty object (a "mount") exactly onto the target object. Then add this component to the "mount" object. Finally, add any objects you want it to follow (e.g. camera) as children to "mount".',
     code: 
-``
+`public Rigidbody target;
+public float turnSpeed = 8;
+
+Vector3 offset;
+Vector3 targetDirection;
+
+void Start () {
+	offset = transform.position - target.transform.position;
+	targetDirection = target.transform.forward;
+}
+
+void FixedUpdate () {
+	if (target != null) {
+		transform.position = target.transform.position + offset;
+		var v = target.velocity;
+		v.y = 0;
+		if (v.sqrMagnitude > 0) {
+			// cannot normalize zero vectors
+			targetDirection = v;
+			targetDirection.Normalize ();
+		}
+		transform.forward = Vector3.Slerp(transform.forward, targetDirection, Time.deltaTime * turnSpeed);
+	}
+}`
   },
   {
     name: '',
